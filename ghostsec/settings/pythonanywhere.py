81c7c395870a45ec8f3c6f5c05782da1aa338011
Anon23261 <anonymous23261@icloud.com>
2024-11-14 21:56:20 -0700

    Fix Django configuration
    - Update PythonAnywhere settings
    - Fix WSGI configuration
    - Simplify Django initialization

--- ghostsec/settings/pythonanywhere.py
+++ ghostsec/settings/pythonanywhere.py
@@ -6,36 +6,47 @@ DEBUG = False
 # Host settings
 ALLOWED_HOSTS = ['anonymous23.pythonanywhere.com']
 
-# Static files configuration
-STATIC_ROOT = '/home/anonymous23/GhostSec/static'
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'ghostsec/static'),
-]
-
-# Media files configuration
-MEDIA_ROOT = '/home/anonymous23/GhostSec/media'
-MEDIA_URL = '/media/'
-
-# Security settings
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_BROWSER_XSS_FILTER = True
-X_FRAME_OPTIONS = 'DENY'
-SECURE_CONTENT_TYPE_NOSNIFF = True
-
-# WhiteNoise configuration
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Database configuration - SQLite for free tier
+# Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ghostsec/static'),
+]
+
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+# Security settings
+SECURE_SSL_REDIRECT = False  # Set to True if you have SSL
+SESSION_COOKIE_SECURE = False  # Set to True if you have SSL
+CSRF_COOKIE_SECURE = False  # Set to True if you have SSL
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Whitenoise settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+INSTALLED_APPS += [
+    'whitenoise.runserver_nostatic',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+] + MIDDLEWARE
 
 # Cache configuration - local memory for free tier
 CACHES = {
@@ -48,11 +59,3 @@ CACHES = {
 # Disable Celery in PythonAnywhere free tier
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
-
-# Email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
--- ghostsec/wsgi.py
+++ ghostsec/wsgi.py
@@ -3,9 +3,15 @@ WSGI config for ghostsec project.
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ghostsec.settings')
+# Add the project directory to the sys.path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ghostsec.settings.pythonanywhere')
 
 application = get_wsgi_application()
