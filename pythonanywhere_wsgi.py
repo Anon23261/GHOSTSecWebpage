323cd130a7f6cd431f6aac02fda94ccdcb0e25c4
Anon23261 <anonymous23261@icloud.com>
2024-11-17 10:38:08 -0700

    Update configuration for PythonAnywhere deployment

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -6,13 +6,17 @@ import os
 import sys
 
 # Add the project directory to the sys.path
-project_home = '/home/anonymous23/GhostSec'
+project_home = '/home/anonymous23/GHOSTSecWebpage'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# Set Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ghostsec.settings.pythonanywhere')
+# Set environment variables
+os.environ['FLASK_ENV'] = 'production'
+os.environ['PYTHONPATH'] = project_home
 
-# Import Django WSGI handler
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+# Load environment variables from .env file
+from dotenv import load_dotenv
+load_dotenv(os.path.join(project_home, '.env'))
+
+# Import Flask application
+from run import app as application
